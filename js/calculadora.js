44b76cc723d2a51b0bd9b9d96c134399c723a9ac
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-03-29 21:29:15 +0200

    Falta %

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,13 +1,64 @@
 var calc = function(){
-
   var equation = "";
-  //Click a los numeros
+  var op1 = "";
+  var op2 = "";
+  var sqr = false;
+  var pow = false;
+
   $(".num").click(function(){
+    if (op1 != ""){
+      op2 = $(this).html();
+    }
     equation += $(this).html();
-    //console.log("eq: " + equation);
-    //$(".display").html(equation);
-    //console.log("disp: " + $(".display").html());
-  });
+    $(".display").val(equation);
+  })
+
+  $(".operator").click(function(){
+    if ($(this).html() == "√"){
+      equation = Math.sqrt(parseFloat(equation));
+      $(".display").val(equation);
+    } else {
+      if ($(this).html() == "x") {
+        equation += "*";
+      } else if ($(this).html() == "÷") {
+        equation += "/";
+      } else if ($(this).html() == "^"){
+        pow = true;
+        if (op1 == ""){
+          op1 = equation;
+        }
+        equation += $(this).html();
+      } else {
+        equation += $(this).html();
+      }
+      $(".display").val(equation);
+    }
+  })
+
+  $(".equals").click(function(){
+    if (pow){
+      pow = false;
+      if (op1 == ""){
+        equation = 0;
+      } else {
+        console.log(op1);
+        console.log(op2);
+        equation = Math.pow(parseFloat(op1),parseFloat(op2));
+      }
+    } else {
+      equation = eval(equation);
+    }
+    $(".display").val(equation);
+    op1 = equation;
+  })
+
+  $(".clear").click(function(){
+    equation = "";
+    op1 = "";
+    op2 = "";
+    $(".display").val(0);
+  })
+
 };
 
 jQuery(document).ready(calc);
