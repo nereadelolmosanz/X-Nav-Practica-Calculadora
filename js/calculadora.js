362a5021610272736b370587ef4441746572dedd
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-04-03 19:21:29 +0200

    teclas añadidas

--- js/calculadora.js
+++ js/calculadora.js
@@ -5,7 +5,6 @@ var calc = function(){
   var ans = "0";
   var sqr = false;
   var pow = false;
-  var perc = false;
 
   $(".num").click(function(){
     if (pow) {
@@ -13,7 +12,7 @@ var calc = function(){
     }
     equation += $(this).html();
     $(".display").val(equation);
-  })
+  });
 
   $(".operator").click(function(){
     if (equation == ""){
@@ -26,7 +25,7 @@ var calc = function(){
       if (ans == NaN) {
         ans = "0";
       }
-      equation = "";    
+      equation = "";
     } else {
       if ($(this).html() == "x") {
         equation += "*";
@@ -41,12 +40,13 @@ var calc = function(){
         equation = ans + "*";
       } else {
         equation += $(this).html();
-      } 
+      }
       $(".display").val(equation);
     }
-  })
+  });
 
-  $(".equals").click(function(){
+
+  function equals(){
     if (pow){
       pow = false;
       ans = Math.pow(parseFloat(ans),parseFloat(exp));
@@ -55,14 +55,133 @@ var calc = function(){
     }
     $(".display").val(ans);
     equation = "";
-  })
+  }
+  $(".equals").click(equals);
 
-  $(".clear").click(function(){
+
+  function clear(){
     equation = "";
     op1 = "";
     op2 = "";
     ans = "";
     $(".display").val(0);
-  })
+  }
+  $(".clear").click(clear);
+
+
+  $(window).keypress(function(e){
+    // Borrar un caracter
+		if(e.which == 8){
+			e.preventDefault();
+	    clear();
+    // Calcular el resultado
+		}else if(e.which == 13){
+			e.preventDefault();
+			equals();
+    // Shift
+		}else if (e.which == 16){
+      if (equation == ""){
+        equation += ans;
+      }
+			$(window).keypress(function(e){
+    		if(e.which == 53){ // %
+          ans = eval(equation)/100;
+          equation = ans + "*";
+        } else if (e.which == 56){
+          equation += '(';
+        } else if (e.which == 57){
+          equation += ')';
+        } else if (e.which == 192){
+          pow = true;
+          console.log(equation);
+          ans = equation;
+          equation += "^";
+          console.log(equation);
+        } else if (e.which == 171){
+          equation += "*";
+        } else if (e.which == 55){
+          equation += "/";
+        }
+      });
+    } else if (e.which == 47 || e.which == 111){
+      if (equation == ""){
+        equation += ans;
+      }
+      equation += '/';
+    } else if (e.which == 42){
+      if (equation == ""){
+        equation += ans;
+      }
+      equation += '*';
+    } else if (e.which == 45 || e.which == 106){
+      if (equation == ""){
+        equation += ans;
+      }
+      equation += '-';
+    } else if (e.which == 43){
+      if (equation == ""){
+        equation += ans;
+      }
+      equation += "+";
+    } else if(e.which == 46){
+      if (equation == ""){
+        equation += ans;
+      }
+			equation += ".";
+    } else if (e.which == 48){
+      if (pow) {
+        exp = 0;
+      }
+      equation += '0';
+    } else if (e.which == 49){
+      if (pow) {
+        exp = 1;
+      }
+      equation += '1';
+    } else if (e.which == 50){
+      if (pow) {
+        exp = 2;
+      }
+      equation += '2';
+    } else if (e.which == 51){
+      if (pow) {
+        exp = 3;
+      }
+      equation += "3";
+    } else if (e.which == 52){
+      if (pow) {
+        exp = 4;
+      }
+      equation += '4';
+    } else if (e.which == 53){
+      if (pow) {
+        exp = 5;
+      }
+      equation += '5';
+    } else if (e.which == 54){
+      if (pow) {
+        exp = 6;
+      }
+      equation += '6';
+    } else if (e.which == 55){
+      if (pow) {
+        exp = 7;
+      }
+      equation += "7";
+    } else if (e.which == 56){
+      if (pow) {
+        exp = 8;
+      }
+      equation += '8';
+    } else if (e.which == 57){
+      if (pow) {
+        exp = 9;
+      }
+      equation += '9';
+    }
+		$("#display").val(equation);
+	});
+
+
 };
 jQuery(document).ready(calc);
